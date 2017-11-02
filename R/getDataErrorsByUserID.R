45db7cbe8f9c6923669163dcb8b969c9dba27365
Thomas Brittain <honeysucklehardware@gmail.com>
2017-11-01 19:57:04 -0500

    Updated documentation: 2017-11-01

--- R/getDataErrorsByUserID.R
+++ R/getDataErrorsByUserID.R
@@ -43,36 +43,71 @@ getDataErrorsByUserID <- function(client,
     hmisFunctionsFilePath <- paste(dropboxPath, "/RScripts/HMIS_R_Functions.R", sep ="")
     source(hmisFunctionsFilePath)
 
-
-
     setError <- function(vector){
         vector <- as.numeric(vector)
         vector <- replace(vector, vector==NA | vector==8 | vector==9 | vector== 99, 255)
         vector
     }
 
-    client <- loadClient(allDataPath)
+    client <- loadClient(allDataPath) 
     client$FirstName <- as.character(client$FirstName)
     client$FirstName[is.na(client$FirstName)] <- "255"
 
     client$LastName <- as.character(client$LastName)
     client$LastName[is.na(client$LastName)] <- "255"
 
-    client$NameDataQuality <- as.numeric(client$NameDataQuality)
-    client$NameDataQuality[is.na(client$NameDataQuality)] <- 255
-    client$NameDataQuality <- setError(client$NameDataQuality)
+    # Convert to number, find SSNs greater than 8 digits.
+    client$SSN <- as.numeric(client$SSN)
+    client$SSN[client$SSN <= 99999999] <- 255
+    client$SSN[is.na(client$SSN)] <- 255
+    
+    client$DOB <- as.character(client$DOB)
+    client$DOB[client$DOB == ''] <- "255"
+    
+    client$DOBDataQuality <- setError(client$DOBDataQuality)
+    
+    simpleParseColumns <- c("SSNDataQuality",
+                            "NameDataQuality",
+                            "DOBDataQuality",
+                            "AmIndAKNative",
+                            "Asian",
+                            "BlackAfAmerican",
+                            "NativeHIOtherPacific",	
+                            "White", 
+                            "RaceNone",
+                            "Ethnicity",	
+                            "Gender",
+                            "VeteranStatus")
 
+    client[simpleParseColumns] <- 
+    lapply(client[simpleParseColumns],function(x){
+        x <- as.numeric(x)
+        x <- replace(x, x==NA | x==8 | x==9 | x== 99, 255)
+        x
+    })
 
-client
-    # enrollment <- loadEnrollment(allDataPath)
-    # exit <- loadExit(allDataPath)
+    # Get a dataframe of all UserIDs
+    errorsByUsers <- data.frame(unique(client$UserID))
+    colnames(errorsByUsers)[1] <- "UserID"
 
-    # allData <- sqldf("SELECT *
-    #                   FROM client a
-    #                   LEFT JOIN enrollment b
-    #                   ON a.PersonalID=b.PersonalID
-    #                   LEFT JOIN exit c
-    #                   ON b.EnrollmentID=c.EnrollmentID")
+    for(column in simpleParseColumns){
+        tmp <- count(client[client[column] == 255,], "UserID")
+        colnames(tmp)[2] <- paste(column, "Errors", sep = "")
+        errorsByUsers <- merge(x = errorsByUsers, y = tmp, by = "UserID", all.x = TRUE)
+    }
 
+    # Also get FirstName, LastName, SSN, DOB
+    complexParseColumns <- c("FirstName", "LastName", "SSN", "DOB")
+    for(column in complexParseColumns){
+        client[column] <- as.character(client[column])
+        tmp <- data.frame(count(client[client[column] == "255",], "UserID"))
+        colnames(tmp)[2] <- paste(column, "Errors", sep = "")
+        errorsByUsers <- merge(x = errorsByUsers, y = tmp, by = "UserID", all.x = TRUE)
+    }
+tmp
+    errorsByUsers[is.na(errorsByUsers)] <- 0
+    errorsByUsersTotal <- aggregate(errorsByUsers$Frequency, by=list(Category=x$Category), FUN=sum)
+    errorsByUsers$TotalErrors <- rowSums(errorsByUsers)
 
+    errorsByUsers[order(-errorsByUsers$TotalErrors),]
 }
